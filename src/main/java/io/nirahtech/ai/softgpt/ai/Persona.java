f6815aa79c814a6da2c1619d1af6a1164d2b5101
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2024-12-03 07:08:53 +0100

    Update project

--- src/main/java/io/nirahtech/ai/softgpt/ai/Persona.java
+++ src/main/java/io/nirahtech/ai/softgpt/ai/Persona.java
@@ -1,5 +1,8 @@
 package io.nirahtech.ai.softgpt.ai;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Collection;
 
 public record Persona(
@@ -10,10 +13,21 @@ public record Persona(
     Collection<String> skills,
     Collection<String> deliverables,
     Collection<String> behavior,
-    Collection<String> instructions
+    Collection<String> instructions,
+    String rowData
 ) {
     @Override
     public final String toString() {
         return "";
-    }   
+    }
+
+    public static final Persona load(final String persona) {
+        return new Persona(null, null, null, null, null, null, null, null, persona);
+    }
+
+    public static final Persona load(final File personaFile) throws IOException {
+        Files.readString(personaFile.toPath());
+        return load(Files.readString(personaFile.toPath()));
+    }
+    
 }
